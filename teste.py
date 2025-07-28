78e0081f86e72c6eeef3ae16989306872a769d6c
Arthur Ramalho <60481981+ramalhow@users.noreply.github.com>
2025-07-28 11:58:30 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -5,18 +5,48 @@ from bibgrafo.aresta import Aresta
 
 from meu_grafo_matriz_adj_nao_dir import MeuGrafo
 
-grafo = GrafoBuilder().tipo(MeuGrafo()).vertices(3).arestas(True).build()
-tam = len(grafo.matriz)
+grafo = GrafoBuilder().tipo(MeuGrafo()) \
+    .vertices([
+    a := Vertice("A"),
+    b := Vertice("B"),
+    c := Vertice("C"),
+    d := Vertice("D"),
+]) \
+    .arestas([
+    Aresta("a1", a, b),
+    Aresta("a2", b, c),
+    Aresta("a3", c, d),
+    Aresta("a4", d, a),
 
-for linhas in grafo.matriz:
-    for dicio in range(tam):
+]) \
+    .build()
+'''
+size = len(grafo.matriz)
+start = 1
+for linha in range(size):
+    for coluna in range(start, size):
+        print(f"coords: {linha}-{coluna}")
+        print(len(grafo.matriz[linha][coluna]))
+
+    start = start + 1
+'''
+
+print(grafo)
+
+nao_adj = set()
+for linha in grafo.matriz:
+    for dicio in linha:
+        print(dicio)
 
         if len(dicio) == 0:
             for aresta in dicio:
                 v1 = aresta.v1.rotulo
                 v2 = aresta.v2.rotulo
-                par = frozenset((v1, v2))
+                nao_adj.add(f"{v1}-{v2}")
 
-                if par not in nao_adj:
-                    nao_adj.add(f"{v1}-{v2}")
-        tam = tam - 1
+
+print(nao_adj)
+'''
+s = grafo.vertices_nao_adjacentes()
+print(s)
+'''
